bc46bd0c1a093e0b3302de54b828161c062a47f6
Jesus Marron <jesusmarron7@gmail.com>
2023-09-26 23:52:15 -0500

    fixed tests

--- lib/telephony/core/prepaid.ex
+++ lib/telephony/core/prepaid.ex
@@ -3,6 +3,7 @@ defmodule Telephony.Core.Prepaid do
   alias Telephony.Core.Recharge
 
   defstruct credits: 0, recharges: []
+
   @price_per_minute 1.45
 
   def make_call(%{subscriber_type: subscriber_type} = subscriber, time_spent, date) do
@@ -13,6 +14,18 @@ defmodule Telephony.Core.Prepaid do
     else
       {:error, "Subscriber does not have sufficient credits"}
     end
+  end
+
+  def recharge(%{subscriber_type: subscriber_type} = subscriber, amount, date) do
+    recharge = Recharge.new(amount, date)
+
+    subscriber_type = %{
+      subscriber_type
+      | recharges: subscriber_type.recharges ++ [recharge],
+        credits: subscriber_type.credits + amount
+    }
+
+    %{subscriber | subscriber_type: subscriber_type}
   end
 
   defp update_credits(%{subscriber_type: subscriber_type} = subscriber, time_spent) do
@@ -28,17 +41,5 @@ defmodule Telephony.Core.Prepaid do
 
   defp has_sufficient_credits?(subscriber_type, time_spent) do
     subscriber_type.credits >= @price_per_minute * time_spent
-  end
-
-  defp recharge(%{subscriber_type: subscriber_type} = subscriber, amount, date) do
-    recharge = Recharge.new(amount, date)
-
-    subscriber_type = %{
-      subscriber_type
-      | recharges: subscriber_type.recharges ++ [recharge],
-        credits: subscriber_type.credits + amount
-    }
-
-    %{subscriber | subscriber_type: subscriber_type}
   end
 end
