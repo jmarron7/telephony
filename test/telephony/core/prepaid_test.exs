c3ce4f3f9b3133b47b0f94a99a4d6e82d057a99a
Jesus Marron <jesusmarron7@gmail.com>
2023-09-27 18:29:56 -0500

    prepaid invoices

--- test/telephony/core/prepaid_test.exs
+++ test/telephony/core/prepaid_test.exs
@@ -1,6 +1,7 @@
 defmodule Telephony.Core.PrepaidTest do
   use ExUnit.Case
   alias Telephony.Core.Call
+  alias Telephony.Core.Invoice
   alias Telephony.Core.Prepaid
   alias Telephony.Core.Recharge
   alias Telephony.Core.Subscriber
@@ -73,5 +74,74 @@ defmodule Telephony.Core.PrepaidTest do
     }
 
     assert expected == result
+  end
+
+  test "print invoice" do
+    date = ~D[2023-07-06]
+    prev_month = ~D[2023-06-09]
+
+    subscriber = %Subscriber{
+      full_name: "John Doe",
+      phone_number: "1234567890",
+      subscriber_type: %Prepaid{
+        credits: 110,
+        recharges: [
+          %Recharge{
+            amount: 100,
+            date: date
+          },
+          %Recharge{
+            amount: 100,
+            date: prev_month
+          },
+          %Recharge{
+            amount: 100,
+            date: prev_month
+          }
+        ]
+      },
+      calls: [
+        %Call{
+          call_duration: 2,
+          date: date
+        },
+        %Call{
+          call_duration: 10,
+          date: prev_month
+        },
+        %Call{
+          call_duration: 20,
+          date: prev_month
+        }
+      ]
+    }
+
+    subscriber_type = subscriber.subscriber_type
+    calls = subscriber.calls
+
+    assert Invoice.print(subscriber_type, calls, 2023, 06) == %{
+             calls: [
+               %{
+                 call_duration: 10,
+                 call_cost: 14.5,
+                 date: prev_month
+               },
+               %{
+                 call_duration: 20,
+                 call_cost: 29.0,
+                 date: prev_month
+               }
+             ],
+             recharges: [
+               %Recharge{
+                 amount: 100,
+                 date: prev_month
+               },
+               %Recharge{
+                 amount: 100,
+                 date: prev_month
+               }
+             ]
+           }
   end
 end
