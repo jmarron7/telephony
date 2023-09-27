028670dce602d804b705076fc7e09899a38d3af9
Jesus Marron <jesusmarron7@gmail.com>
2023-09-27 17:27:51 -0500

    added subscriber functionality and test

--- lib/telephony/core/subscriber.ex
+++ lib/telephony/core/subscriber.ex
@@ -12,4 +12,23 @@ defmodule Telephony.Core.Subscriber do
     payload = %{payload | subscriber_type: %Postpaid{}}
     struct(__MODULE__, payload)
   end
+
+  def make_call(%{subscriber_type: subscriber_type} = subscriber, call_duration, date)
+      when subscriber_type.__struct__ == Postpaid do
+    Postpaid.make_call(subscriber, call_duration, date)
+  end
+
+  def make_call(%{subscriber_type: subscriber_type} = subscriber, call_duration, date)
+      when subscriber_type.__struct__ == Prepaid do
+    Prepaid.make_call(subscriber, call_duration, date)
+  end
+
+  def recharge(%{subscriber_type: subscriber_type} = subscriber, amount, date)
+      when subscriber_type.__struct__ == Prepaid do
+    Prepaid.recharge(subscriber, amount, date)
+  end
+
+  def recharge(_, _, _) do
+    {:error, "Recharges can only be applied to prepaid accounts"}
+  end
 end
